f70478c878a1d401909780e5e5e73b8fed632964
Jacob Archambault <jacobarchambault@gmail.com>
2022-07-01 16:56:09 -0400

    Separate out ConsoleS3Client class

---
+++ ConsoleS3Client.cs
@@ -0,0 +1,34 @@
+using Amazon.S3;
+
+namespace S3CreateAndList
+{
+    class ConsoleS3Client 
+    {
+        private readonly AmazonS3Client s3Client = new AmazonS3Client();
+        internal async Task ListBuckets()
+        {
+            Console.WriteLine("\nGetting a list of your buckets...");
+            var bucketNames = (await s3Client.ListBucketsAsync()).Buckets.Select(x => x.BucketName);
+            Console.WriteLine($"Number of buckets: {bucketNames.Count()}");
+            foreach (var b in bucketNames)
+            {
+                Console.WriteLine(b);
+            }
+        }
+
+        internal async Task TryPutBucket(string bucketName)
+        {
+            try
+            {
+                Console.WriteLine($"\nCreating bucket {bucketName}...");
+                Console.WriteLine($"Result: {(await s3Client.PutBucketAsync(bucketName)).HttpStatusCode.ToString()}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Caught exception when creating a bucket:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -25,37 +25,12 @@ namespace S3CreateAndList
             {
                 Console.WriteLine("\nNo arguments specified. Will simply list your Amazon S3 buckets." + "\nIf you wish to create a bucket, supply a valid, globally unique bucket name.");
             }
-            var s3Client = new AmazonS3Client();
+            var s3Client = new ConsoleS3Client();
             if (args.Length == 1)
             {
-                await TryPutBucket(args, s3Client);
+                await s3Client.TryPutBucket(args[0]);
             }
-            await ListBuckets(s3Client);
-        }
-
-        private static async Task ListBuckets(AmazonS3Client s3Client)
-        {
-            Console.WriteLine("\nGetting a list of your buckets...");
-            var bucketNames = (await s3Client.ListBucketsAsync()).Buckets.Select(x => x.BucketName);
-            Console.WriteLine($"Number of buckets: {bucketNames.Count()}");
-            foreach (var b in bucketNames)
-            {
-                Console.WriteLine(b);
-            }
-        }
-
-        private static async Task TryPutBucket(string[] args, AmazonS3Client s3Client)
-        {
-            try
-            {
-                Console.WriteLine($"\nCreating bucket {args[0]}...");
-                Console.WriteLine($"Result: {(await s3Client.PutBucketAsync(args[0])).HttpStatusCode.ToString()}");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Caught exception when creating a bucket:");
-                Console.WriteLine(e.Message);
-            }
+            await s3Client.ListBuckets();
         }
     }
 }
