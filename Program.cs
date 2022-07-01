142a7d9298a6249c8ea47d11fe914be5a9b8eee9
Jacob Archambault <jacobarchambault@gmail.com>
2022-07-01 16:42:28 -0400

    Separate out TryPutBucket and ListBuckets logic into private static methods

--- Program.cs
+++ Program.cs
@@ -25,33 +25,36 @@ namespace S3CreateAndList
             {
                 Console.WriteLine("\nNo arguments specified. Will simply list your Amazon S3 buckets." + "\nIf you wish to create a bucket, supply a valid, globally unique bucket name.");
             }
-            // Create an S3 client object.
             var s3Client = new AmazonS3Client();
             if (args.Length == 1)
             {
-                // If a bucket name was supplied, create the bucket.
-                // Call the API method directly
-                try
-                {
-                    Console.WriteLine($"\nCreating bucket {args[0]}...");
-                    Console.WriteLine($"Result: {(await s3Client.PutBucketAsync(args[0])).HttpStatusCode.ToString()}");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Caught exception when creating a bucket:");
-                    Console.WriteLine(e.Message);
-                }
-
+                await TryPutBucket(args, s3Client);
             }
+            await ListBuckets(s3Client);
+        }
 
-            // List the buckets owned by the user.
-            // Call a class method that calls the API method.
+        private static async Task ListBuckets(AmazonS3Client s3Client)
+        {
             Console.WriteLine("\nGetting a list of your buckets...");
             var bucketNames = (await s3Client.ListBucketsAsync()).Buckets.Select(x => x.BucketName);
             Console.WriteLine($"Number of buckets: {bucketNames.Count()}");
             foreach (var b in bucketNames)
             {
                 Console.WriteLine(b);
+            }
+        }
+
+        private static async Task TryPutBucket(string[] args, AmazonS3Client s3Client)
+        {
+            try
+            {
+                Console.WriteLine($"\nCreating bucket {args[0]}...");
+                Console.WriteLine($"Result: {(await s3Client.PutBucketAsync(args[0])).HttpStatusCode.ToString()}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Caught exception when creating a bucket:");
+                Console.WriteLine(e.Message);
             }
         }
     }
