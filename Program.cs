6132c6196c118fa1627cb6873c3781436e0650e9
Jacob Archambault <jacobarchambault@gmail.com>
2022-07-01 13:25:33 -0400

    Move GetBucketName into its own class

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace S3CreateAndList
             var s3Client = new AmazonS3Client();
 
             // Parse the command line arguments for the bucket name.
-            if (GetBucketName(args, out String bucketName))
+            if (BucketName.GetBucketName(args, out String bucketName))
             {
                 // If a bucket name was supplied, create the bucket.
                 // Call the API method directly
@@ -48,38 +48,6 @@ namespace S3CreateAndList
                 Console.WriteLine(b.BucketName);
             }
         }
-
-
-        // 
-        // Method to parse the command line.
-        private static Boolean GetBucketName(string[] args, out String bucketName)
-        {
-            Boolean retval = false;
-            bucketName = String.Empty;
-            if (args.Length == 0)
-            {
-                Console.WriteLine("\nNo arguments specified. Will simply list your Amazon S3 buckets." +
-                  "\nIf you wish to create a bucket, supply a valid, globally unique bucket name.");
-                bucketName = String.Empty;
-                retval = false;
-            }
-            else if (args.Length == 1)
-            {
-                bucketName = args[0];
-                retval = true;
-            }
-            else
-            {
-                Console.WriteLine("\nToo many arguments specified." +
-                  "\n\ndotnet_tutorials - A utility to list your Amazon S3 buckets and optionally create a new one." +
-                  "\n\nUsage: S3CreateAndList [bucket_name]" +
-                  "\n - bucket_name: A valid, globally unique bucket name." +
-                  "\n - If bucket_name isn't supplied, this utility simply lists your buckets.");
-                Environment.Exit(1);
-            }
-            return retval;
-        }
-
 
     }
 }
